756d73c75ecb1db043543fbfd4092b9781a190f6
Benaka Moorthi <benaka.moorthi@mheducation.com>
2016-06-25 23:09:32 -0700

    Added CLI frontend w/ logic (untested) + stubbed link functionality & stubbed docker-compose generation.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 'use strict'
 
 module.exports = {
-  extends: "airbnb-base",
+  extends: 'airbnb-base',
   plugins: [],
   parserOptions: {
     sourceType: 'script',
   },
   rules: {
-    semi: [2, "never"],
-    "no-use-before-define": ["error", { "functions": false, "classes": true }],
-    "prefer-arrow-callback": 0,
+    semi: [2, 'never'],
+    'no-use-before-define': ['error', { 'functions': false, 'classes': true }],
+    'prefer-arrow-callback': 0,
+    'no-console': 0,
   },
   env: {
     node: true,
--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,11 +1,11 @@
 'use strict'
 
 module.exports = {
-  extends: "../.eslintrc.js",
+  extends: '../.eslintrc.js',
   rules: {
-    "func-names": 0,
-    "no-unused-expressions": 0,
-    "max-len": ["error", 120],
+    'func-names': 0,
+    'no-unused-expressions': 0,
+    'max-len': ['error', 120],
   },
   env: {
     node: true,
